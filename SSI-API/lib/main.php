b10a1f7b6e4813e5c3586a1f5f1edf40b036a2c9
Robert Colca <robery_office@yahoo.ro>
2015-06-01 17:35:24 +0300

    Bugfix

--- SSI-API/lib/main.php
+++ SSI-API/lib/main.php
@@ -8,6 +8,7 @@ Class Actiune {
   }
 
   public function user_exists($user_id = NULL) {
+    $DB = $this->db;
     return $DB->query("SELECT * FROM users WHERE user_id='{$user_id}'")->num_rows;
   }
 
