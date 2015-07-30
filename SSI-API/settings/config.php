aa4b3e2abd1134f42ac8a63f01465bc9c9e876e0
Robert Colca <robery_office@yahoo.ro>
2015-07-30 19:19:58 +0300

    Implemented MongoDB

--- SSI-API/settings/config.php
+++ SSI-API/settings/config.php
@@ -1,6 +1,7 @@
 <?php
 
 define ('development_mode', true);
+define ('mongodb_store', true); // If enabled the images will be stored in a mongodb table
 
 $mysql = [
     'driver' => 'mysqli',  // Available types: mysqli
--- SSI-API/ssi_api.php
+++ SSI-API/ssi_api.php
@@ -9,14 +9,20 @@
 */
 namespace SSI\DB\Connection;
 
-include 'lib/main.php';
-include 'settings/config.php';
+require_once __DIR__.'/lib/main.php';
+require_once __DIR__.'/settings/config.php';;
 
 use SSI\Lib\Main\Actiune as Actiune;
 
 development_mode ? error_reporting(E_ALL) : error_reporting(0);
 
 try {
+	// Initializing the MongoDB Connection if enabled
+	if (mongodb_store && class_exists('MongoClient')) {
+		$mongo = new MongoClient();
+		$mongo_db = $mongo->users;
+	}
+
 	switch ($mysql['driver']) {
 		case 'mysqli':
 			$DB = @new \MySQLi ( $mysql ['hostname'], $mysql ['username'], $mysql ['password'], $mysql ['database'] );
@@ -29,35 +35,35 @@ try {
 				if(!$operation->check_database($mysql['database'])) {
 					throw new \Exception("DATABASE_CORUPT");
 				} else {
-					$action  = isset($_GET['action'])  ? $DB->real_escape_string($_GET['action']) : NULL;
-					$email = isset($_GET['email']) ? $DB->real_escape_string($_GET['email']) : NULL;
-					$fbid = isset($_GET['fbid']) ? $DB->real_escape_string($_GET['fbid']) : NULL;
+					$action = isset($_GET['action']) ? $DB->real_escape_string($_GET['action']) : NULL;
+					$email  = isset($_GET['email'])  ? $DB->real_escape_string($_GET['email'])  : NULL;
+					$fbid   = isset($_GET['fbid'])   ? $DB->real_escape_string($_GET['fbid'])   : NULL;
 
 					/*
 						@Param: action
-						
+
 						@Param Type : GET
-						
+
 						@Catchable exceptions:
 							* INVALID_ACTION if the provided action is invalid
-						
+
 					*/
 					switch ($action) {
-						
-						/* 
+
+						/*
 							@Action Name: get_info (action=get_info)
-							
+
 							@Params:
 								* email (STRING)
-							
-							@Params Type: POST	
-							
-							@Output : 
+
+							@Params Type: POST
+
+							@Output :
 								* User details for a provided email address
-								
-							@Catchable exceptions:	
+
+							@Catchable exceptions:
 								* NOT_FOUND if the provided email is not in the database
-						*/						
+						*/
 						case 'get_info':
 							if (!$operation->user_exists($email, $fbid))
 									throw new \Exception("NOT_FOUND");
@@ -66,23 +72,23 @@ try {
 								exit($data);
 							}
 						break;
-						
+
 						case 'status_check':
 						    exit("SUCCESS");
-						/* 
+						/*
 							@Action Name: get_events (action=get_events)
-							
+
 							@Params:
 								* email (STRING)
-							
-							@Params Type: POST	
-							
-							@Output : 
+
+							@Params Type: POST
+
+							@Output :
 								* User events for a provided email address
-								
+
 							@Catchable exceptions:
 								* NOT_FOUND if the provided email is not in the database
-						*/	
+						*/
 						case 'get_events':
 							if (!$operation->user_exists($email, $fbid))
 									throw new \Exception("NOT_FOUND");
@@ -92,71 +98,71 @@ try {
 							}
 						break;
 
-						/* 
+						/*
 							@Action Name: insert_event (action=insert_event)
-							
+
 							@Params:
 								* fbid (INT) - CAN BE NULL
 								* email (STRING)
 								* date (DATE)
 								* data (JSON)
-								
-							@Params Type: POST	
-							
-							@Output : 
+
+							@Params Type: POST
+
+							@Output :
 								* true  (1) on success
 								* false (0) on failure
-								
+
 							@Catchable exceptions:
 								* INCORRECT_CREDENTIALS if the provided (fbid AND email) are invalid
-						*/	
+						*/
 						case 'insert_event':
 							$data = [
-								'fbid' 			=> isset($_POST['fbid']) ? $DB->real_escape_string($_POST['fbid']) : NULL,
-								'email'	 		=> isset($_POST['email'])  ? $DB->real_escape_string($_POST['email'])  : NULL,
+								'fbid' 			=> isset($_POST['fbid'])  ? $DB->real_escape_string($_POST['fbid'])  : NULL,
+								'email'	 		=> isset($_POST['email']) ? $DB->real_escape_string($_POST['email']) : NULL,
 								'date' 			=> isset($_POST['date'])  ? $DB->real_escape_string($_POST['date'])  : NULL,
 								'data' 			=> isset($_POST['data'])  ? $DB->real_escape_string($_POST['data'])  : NULL,
-								'num'           => isset($_POST['num']) ? $DB->real_escape_string($_POST['num'])   : NULL,
+								'num'           => isset($_POST['num'])   ? $DB->real_escape_string($_POST['num'])   : NULL,
 							];
 
 							if ($operation->user_exists($data['email'], $data['fbid']) && !$operation->event_exists($data['email']), $data['date'])
 								echo $operation->insert_event($data['email'], $data['date'], $data['data'], $data['num']) ? 1 : 0;
 							else if ($operation->user_exists($data['email'], $data['fbid']) && $operation->event_exists($data['email']), $data['date'])
 								echo $operation->alter_event($data['email'], $data['date'], $data['data'], $data['num']) ? 1 : 0;
-			
+
 							else
 								throw new \Exception ("INCORRECT_CREDENTIALS");
 						break;
 
-						/* 
+						/*
 							@Action Name: insert (action=insert)
-							
+
 							@Params:
 								* fbid (INT)
 								* email (STRING)
 								* name (STRING)
 								* image (STRING)
 								* password (STRING)
-								
-							@Params Type: POST	
-								
-							@Output : 
+
+							@Params Type: POST
+
+							@Output :
 								* true  (1) on success
 								* false (0) on failure
-								
+
 							@Catchable exceptions:
 								* INVALID_EMAIL if the provided email is invalid
 								* INVALID_NAME if the provided name is invalid
 								* INVALID_FBID if the provided fbid is invalid
 								* USER_EXISTS if the user already exists
-						*/	
+						*/
 						case 'insert':
 							$data = [
 								'fbid'  		=> isset($_POST['fbid'])     ? $DB->real_escape_string($_POST['fbid'])     : NULL,
 								'email' 		=> isset($_POST['email'])    ? $DB->real_escape_string($_POST['email'])    : NULL,
 								'name'  		=> isset($_POST['name'])     ? $DB->real_escape_string($_POST['name'])     : NULL,
 								'image' 		=> isset($_POST['image'])    ? $DB->real_escape_string($_POST['image'])    : NULL,
-								'password'  => isset($_POST['password'])     ? $DB->real_escape_string($_POST['password']) : NULL
+								'password'  => isset($_POST['password']) ? $DB->real_escape_string($_POST['password']) : NULL
 							];
 
 							if (!$operation->user_exists($data['email'], $data['fbid'])) {
@@ -165,9 +171,14 @@ try {
 								else if (is_null($data['name']) || strlen($data['name']) < 3)
 									throw new \Exception("INVALID_NAME");
 
-								if (!is_null($data['name']) && filter_var($data['email'], FILTER_VALIDATE_EMAIL))
-								{
-									echo $operation->insert_user($data['fbid'], $data['email'], $data['name'], $data['image'], $data['password']) ? 1 : 0;
+								if (!is_null($data['name']) && filter_var($data['email'], FILTER_VALIDATE_EMAIL))	{
+									if (mongodb_store && class_exists('MongoClient')) {
+										echo $operation->insert_user($data['fbid'], $data['email'], $data['name'], $data['image'], $data['password']) ? 1 : 0;
+									} else
+											echo $operation->insert_user($data['fbid'], $data['email'], $data['name'], NULL, $data['password']) ? 1 : 0;
+											$collection = $db->$data['email'].$data['fbid']."_image";
+											$document = array( "email" => $data['email'], "image" => $data['image'] );
+											$collection->insert($document);
 								}
 							} else if ($operation->user_exists($data['email'], $data['fbid']) == 2) {
 									if (is_null($data['fbid']))
@@ -184,32 +195,32 @@ try {
 								throw new \Exception("USER_EXISTS");
 							}
 						break;
-						
-						/* 
+
+						/*
 							@Action Name: login (action=login)
-							
+
 							@Params:
 								* password (STRING)
 								* email (STRING)
-							
-							@Params Type: POST							
-							
-							@Output : 
+
+							@Params Type: POST
+
+							@Output :
 								* SUCCESS if the provided are credentials are correct
-								
+
 							@Catchable exceptions:
 								* INCORRECT_CREDENTIALS if the provided credentials are not correct
-						*/							
+						*/
 						case 'login':
 							$email = isset($_POST['email']) ? $DB->real_escape_string($_POST['email']) : NULL;
 							$password = isset($_POST['password']) ? $DB->real_escape_string($_POST['password']) : NULL;
-							
+
 							if($operation->check_password($email, $password))
 								exit("SUCCESS");
-							else						
+							else
 								throw new \Exception("INCORRECT_CREDENTIALS");
 						break;
-						
+
 						/*
 						 TODO:
 						*/
@@ -218,42 +229,42 @@ try {
 								'fbid'  		=> isset($_POST['fbid'])     ? $DB->real_escape_string($_POST['fbid'])     : NULL,
 								'email' 		=> isset($_POST['email'])    ? $DB->real_escape_string($_POST['email'])    : NULL
 							];
-							
+
 							if (!$operation->user_exists($data['email'], $data['fbid'])) {
 								throw new \Exception("ERR_404");
 							} else {
-								
+
 							}
-							
+
 						break;
-						
-						/* 
+
+						/*
 							@Action Name: get_user_event (action=get_user_event)
-							
+
 							@Params:
 								* date (DATE)
 								* email (STRING)
-							
+
 							@Params Type: GET
-							
-							@Output : 
+
+							@Output :
 								* all the user_events from the specific date
-								
+
 							@Catchable exceptions:
 								* NOT_FOUND if the provided email doesn't exist in the database
-						*/	
+						*/
 						case 'get_user_event':
 							$email	 	= isset($_GET['email'])  ? $DB->real_escape_string($_GET['email'])  : NULL;
 							$date 		= isset($_GET['date'])  ? $DB->real_escape_string($_GET['date'])  : "1.1.2015";
-							
+
 							if (!$operation->user_exists($email))
 									throw new \Exception("NOT_FOUND");
-							else { 
+							else {
 								$data = json_encode($operation->get_user_event($email,$date));
 								exit($data);
 							}
 						 break;
-							
+
 
 						default:
 							throw new \Exception("INVALID_ACTION");
