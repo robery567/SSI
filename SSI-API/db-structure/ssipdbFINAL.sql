6be75f01c5a04029847bbf427050e8ccd9c32709
Alexie Dariciuc <dariciucalexie@gmail.com>
2015-07-31 23:14:48 +0300

    dbstruct

---
+++ SSI-API/db-structure/ssipdbFINAL.sql
@@ -0,0 +1,46 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.0.20-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.1.0.4867
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping database structure for ssipdb
+DROP DATABASE IF EXISTS `ssipdb`;
+CREATE DATABASE IF NOT EXISTS `ssipdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `ssipdb`;
+
+
+-- Dumping structure for table ssipdb.events
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE IF NOT EXISTS `events` (
+  `num` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `data` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table ssipdb.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fbid` varchar(50) DEFAULT NULL,
+  `email` varchar(250) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `password` text NOT NULL,
+  `image` longtext,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
